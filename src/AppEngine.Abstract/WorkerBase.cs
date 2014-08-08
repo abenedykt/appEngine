445dbf326d7323521c72351699dfffb20d82f4eb
abenedykt <arkadiusz@benedykt.net>
2014-08-08 16:17:07 +0200

    clean up

--- src/AppEngine.Abstract/WorkerBase.cs
+++ src/AppEngine.Abstract/WorkerBase.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AppEngine.Base
+﻿namespace AB.AppEngine.Abstract
 {
 	public abstract class WorkerBase<TRequest, TResponse> : IHandle<TRequest, TResponse>
 	{
